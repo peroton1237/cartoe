24ca6b4acacc9b24877c417c86c5c5ad658cf257
peroton1237 <144845100+peroton1237@users.noreply.github.com>
2024-09-25 10:50:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Java?',
+    'O Java é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma linha de codigo?',
+    'Uma linha de código é uma linha que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz ola em Inglês?',
+    'Oi em ingles é Hello'
+)
